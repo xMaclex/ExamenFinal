3381f32a301ddf381be137d6da0d7ca75acb5448
xMaclex <62273275+xMaclex@users.noreply.github.com>
2020-08-05 00:25:51 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,95 +1,94 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace Proyectofinal
-{
-    class User
+class Usuario
     {
         public string Nombre;
-        public string UserName;
-        public int Password;
-        public string Cargo;
-        public string FechaCreacion;
-        public bool Estado;
-
+        public string Cedula;
+        public string Rol;
+        public string Fecha;
+        public string Estado;
+        public int Contraseña;
     }
-
     class Program
     {
-
         static void Main(string[] args)
         {
 
-            bool condicion = false;
-            bool condicionAll = false;
-            string UserName;
-            int Contraseña;
-            do
-            {
+                Usuario user1 = new Usuario();
+                Usuario user2 = new Usuario();
+                Usuario user3 = new Usuario();
+                Random rnd = new Random();
+                string cedula;
+                int Contraseña;
+                bool fallos = false;
+                string[] Estados = { "activo", "inactivo" };
                 do
                 {
-                    Console.Clear();
-
-                    //bool condicion;
-
-                    Console.Write("User Name: ");
-                    UserName = Console.ReadLine();
-                    Console.Write("\nContreseña: ");
-                    condicion = int.TryParse(Console.ReadLine(), out Contraseña);
-                    if (condicion == false)
+                    Console.WriteLine("cedula:");
+                    cedula = Console.ReadLine();
+                    Console.WriteLine("contrase;a");
+                    fallos = int.TryParse(Console.ReadLine(), out Contraseña);
+                    if (fallos == false)
                     {
                         Console.WriteLine("Las contraseñas son numericas!, vuelve a intentarlo.");
                         Console.ReadKey();
                     }
 
-                } while (condicion == false);
+                } while (fallos == false);
 
 
-                User users1 = new User();
-                User users2 = new User();
-                User users3 = new User();
-                List<User> listaUsers = new List<User>();
-                users1.Nombre = "xMaclex";
-                users1.UserName = "402-2871667-2";
-                users1.Password = 123456;
-                users1.Cargo = "Supervisor";
-                users1.FechaCreacion = "11/04/2015";
-                users1.Estado = true;
-                listaUsers.Add(users1);
+                List<Usuario> lista = new List<Usuario>();
+                user1.Nombre = "xMaclex";
+                user1.Contraseña = 2019;
+                user1.Cedula = "402-2871667-2";
+                user1.Rol = "Administrador";
+                user1.Fecha = "14/07/2019";
+                user1.Estado = "";
+                lista.Add(user1);
 
-                users2.Nombre = "Alejo";
-                users2.UserName = "402-8804567-2";
-                users2.Password = 123;
-                users2.Cargo = "Administrador";
-                users2.FechaCreacion = "25/06/2020";
-                users2.Estado = true;
-                listaUsers.Add(users2);
 
-                users3.Nombre = "xMorenax";
-                users3.UserName = "402-2978667-2";
-                users3.Password = 1234;
-                users3.Cargo = "Vendedor";
-                users3.FechaCreacion = "11/04/2020";
-                users3.Estado = false;
-                listaUsers.Add(users3);
+                user2.Nombre = "SR.Pedro";
+                user2.Contraseña = 2019;
+                user2.Cedula = "402-1566781-3";
+                user2.Rol = "Supervisor";
+                user2.Fecha = "09/09/2019";
+                user2.Estado = "";
+                lista.Add(user2);
 
+                user3.Nombre = "Sketch";
+                user3.Contraseña = 2019;
+                user3.Cedula = "402-4670119-9";
+                user3.Rol = "Vendedor";
+                user3.Fecha = "23/01/2020";
+                user3.Estado = "";
+                lista.Add(user3);
+
+                
+                
+
+                for (int a = 0; a < 3; a++)
+                {
+                    int Indice = rnd.Next(Estados.Length);
+
+                    lista[a].Estado = Estados[Indice];
+                   
+                }
 
                 for (int i = 0; i < 3; i++)
                 {
-                    string User = listaUsers[i].UserName;
-                    int Password = listaUsers[i].Password;
-                    string estado;
-                    if (UserName == User && Contraseña == Password && listaUsers[i].Estado == true)
+                    string UserName = lista[i].Cedula;
+                    int Password = lista[i].Contraseña;
+                    string estado = lista[i].Estado;
+                    if (UserName == cedula && Contraseña == Password && estado == "activo")
                     {
-                        estado = "Activo";
                         Console.Clear();
-                        Console.WriteLine("Bienvenido {0}!, Tu cargo es {1}", listaUsers[i].Nombre, listaUsers[i].Cargo);
-                        Console.WriteLine("User: {0}.\nUserName: {1}. \nCargo: {2}.\nFecha de Creación: {3}.\nEstado: Activo.", listaUsers[i].Nombre, UserName, listaUsers[i].Cargo, listaUsers[i].FechaCreacion);
+                        Console.WriteLine("Bienvenido {0}!, Tu cargo es {1}", lista[i].Nombre, lista[i].Rol);
+                        Console.WriteLine("User: {0}.\nUserName: {1}. \nCargo: {2}.\nFecha de Creación: {3}.\nEstado: Activo.", lista[i].Nombre, UserName, lista[i].Rol, lista[i].Fecha);
                         Console.ReadKey();
-                        condicionAll = true;
                         break;
                     }
-                    else if (UserName == User && Contraseña == Password && listaUsers[i].Estado == false)
+                    else if (UserName == cedula && Contraseña == Password && lista[i].Estado == "inactivo")
                     {
                         Console.WriteLine("Lo sentimos, el Usuario {0} está inactivo.", UserName);
                         Console.WriteLine("Intenta con otro.");
@@ -97,11 +96,5 @@ namespace Proyectofinal
                     }
 
                 }
-            } while (condicionAll == false);
-
         }
-
     }
-
-
-}
